eca206b8134615ad018e095684b1ecdad17cc6cd
subash karki <karki011@gmail.com>
2018-05-02 19:03:29 -0500

    learned props, state, inline conditions

--- moviedbs/src/App.js
+++ moviedbs/src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +24,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        { this.state.toggle && 
+          <p> This should show and hide.</p>
+        }
+        <button onClick={this.toggle}> Show / Hide </button>
       </div>
     );
   }
