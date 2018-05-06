58feb3841a96b3b5b1485401c4ba6c665a6d8ebc
subash karki <karki011@gmail.com>
2018-05-05 20:39:04 -0500

    movie iteration

--- moviedbs/src/App.js
+++ moviedbs/src/App.js
@@ -1,61 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Start Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider man'
+  },
+  {
+    id: 3,
+    title: '36th Camber of shaloin'
+  }
+];
 
 class App extends Component {
-
-  // lifecycle method steps
-
-  constructor(props){
-    super(props);
-    console.log("constructor");
-  }
-
-  componentWillMount(){ //dont use it at often it wont trigger rerender
-    console.log("willmount");
-  }
-
-  componentDidMount(){//state will change and call rerender
-    console.log('mounted');
-  }
-  //
-  state = {
-    toggle: true
-  }
-
-  toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    })
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-         <Welcome />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        { this.state.toggle && 
-          <p> This should show and hide.</p>
-        }
-        <button onClick={this.toggle}> Show / Hide </button>
+        </header>  
+
+        {/* iteration  */}
+        {movies.map((movie) =>  <Movie key={movie.id} movie ={movie}/>)}   
+      {/* iteration  */}
       </div>
     );
   }
 }
 
-
-class Welcome extends Component {
-  render(){
-    return (
-      <h1 className="App-title">Welcome to React</h1>
-
-    )
-  }
-}
 
 export default App;
