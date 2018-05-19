ee999ab548cd0d5cade982b0ba1fa0828f057cfd
Subash karki <karki011@gmail.com>
2018-05-19 09:57:44 -0500

    css added

--- moviedbs/src/App.js
+++ moviedbs/src/App.js
@@ -3,21 +3,25 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Link,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import MoviesList from './MoviesList';
 
+import MovieDetails from './MovieDetails';
 
 const App = () => (
   <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
       </header>
       <Switch>
         <Route exact path="/" component={MoviesList} />
-        <Route exact path="/test" component={MoviesList} />
+        <Route path="/:id" component={MovieDetails} />
       </Switch>
     </div>
   </Router>
--- moviedbs/src/MoviesList.js
+++ moviedbs/src/MoviesList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-
+import styled from 'styled-components';
 import Movie from './Movie';
 
 
@@ -22,12 +21,18 @@ class MoviesList extends Component {
 
   render() {
     return (
-      <div>
+      <MovieGrid>
         {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      </MovieGrid>
     );
   }
 }
 
 export default MoviesList;
 
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(5, 1fr);
+  grid-row-gap: 1rem;
+`;
