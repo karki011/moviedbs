a2ac14b474dd69f82d8a1a37f82f5428c6116e06
Subash Karki <skarki@subashs-mbp.lan>
2018-05-18 20:47:03 -0500

    react component

--- moviedbs/src/App.js
+++ moviedbs/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
 
 class App extends Component {
+  state = {
+    movies: [],
+  }
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=883fa5a00cd1929226900d5fc30aed4f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({ // eslint-disable-line
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
---
+++ moviedbs/src/Movie.js
@@ -0,0 +1,19 @@
+import React , {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class Movie extends Component {
+    static PropTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3> { this.props.movie.title }</h3>
+            </div>
+        )
+    }
+}
